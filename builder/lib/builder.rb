215a3218f48e397facc4e9650950659a861c514d
Bob <140177710+TheBigLettuce@users.noreply.github.com>
2026-03-05 15:14:41 +0800

    remove builder

--- builder/lib/builder.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "builder/version"
-
-module Builder
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- builder/lib/common/raise_on_error.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-def raise_on_error(out)
-  raise out.first.to_s if out.last.exitstatus != 0
-end
